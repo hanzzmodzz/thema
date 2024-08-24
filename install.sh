f3614da9eb00c6667516d487ac57e8f6767de89a
hanzzmodzz <karpetmelayang321@gmail.com>
2024-08-25 01:32:42 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -62,7 +62,7 @@ check_token() {
   echo -e "${YELLOW}MASUKAN AKSES TOKEN :${NC}"
   read -r USER_TOKEN
 
-  if [ "$USER_TOKEN" = "Hanzmodzz" ]; then
+  if [ "$USER_TOKEN" = "Hanzoffcxmodzz" ]; then
     echo -e "${GREEN}AKSES BERHASIL${NC}}"
   else
     echo -e "${GREEN}Buy dulu Gih Ke Hanzmodzz${NC}"
